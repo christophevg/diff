ac46537923f8b8a69360494f853704e81cb6fdd0
Christophe VG <xtof@astroboy.local>
2012-09-19 14:51:35 +0200

    refactored functionality into classes

--- diff.php
+++ diff.php
@@ -1,94 +1,210 @@
 <?php
 
-const SAME = 1;
-const DEL  = 2;
-const ADD  = 3;
+/**
+* PHP implementation of a line-based diff algorithm
+*/
 
-// looks for a line in an array starting at an index inceased with an offset
-function look_for( $line, $array, $start, $offset ) {
-  for( $d=$start; $d<count($array)-$offset; $d++ ) {
-    if( $array[$d+$offset] == $line ) {
-      return $d;
+class diff {
+  private $oldLines = array();
+  private $newLines = array();
+  private $context  = 3;
+
+  private $actions  = array();
+  
+  private $lines = array();
+  private $line = -1;
+  
+  const ALL = -1;
+  
+  const SAME = 1;
+  const DEL  = 2;
+  const ADD  = 3;
+  
+  public function __construct( $old, $new ) {
+    $this->oldLines = split( "\n", $old );
+    $this->newLines = split( "\n", $new );
+    $this->determineActions();
+  }
+  
+  public function showContext($context = self::ALL) {
+    $this->context = $context;
+    $this->prepareLines();
+    return $this;
+  }
+
+  public function getNextLine() {
+    if( $this->line >= count($this->lines)-1 ) { return false; }
+    $this->line++;
+    return $this->lines[$this->line];
+  }
+
+  private function determineActions() {
+    $actions_add = $this->diff( self::ADD );
+    $actions_del = $this->diff( self::DEL );
+    $this->actions = count($actions_add) < count( $actions_del) 
+      ? $actions_add : $actions_del;
+  }
+  
+  private function prepareLines() {
+    $this->lines = array();
+    $last = count($this->actions) - 1;
+    $i = 0;
+    $o = 0; $n = 0;
+    while( $i <= $last ) {
+      if( $this->actions[$i] == self::ADD ) {
+        $this->lines[] = new diffLine( 'add', $this->newLines[$n], null, $n );
+        $n++;
+        $i++;
+      } elseif( $this->actions[$i] == self::DEL ) {
+        $this->lines[] = new diffLine( 'del', $this->oldLines[$o], $o, null );
+        $o++;
+        $i++;
+      } else { // SAME
+        // find end of SAME block
+        $end = $i;
+        while( $end <= $last && $this->actions[$end+1] == self::SAME ) { $end++; }
+        if( $this->context == self::ALL || ($end - $i) < $this->context * 2 ) {
+          // all of SAME block is context, nothing to skip
+          while( $i<=$end ) {
+            $i++;
+            $this->lines[] = new diffLine( 'context', $this->newLines[$n], $o, $n );
+            $o++; $n++;
+          }
+        } else { // context / skip / context
+          for( $c=0; $c<$this->context; $c++ ) {
+            $this->lines[] = new diffLine( 'context', $this->newLines[$n], $o, $n );
+            $o++; $n++;
+            $i++;
+          }
+          $this->lines[] = new diffLine( 'skip', '...', null, null );
+          $skip = $end - $i - $this->context + 1;
+          $o += $skip; $n += $skip;
+          $i += $skip;
+          for( $c=0; $c<$this->context; $c++ ) {
+            $this->lines[] = new diffLine( 'context', $this->newLines[$n], $o, $n );
+            $o++; $n++;
+            $i++;
+          }
+        }
+      }
     }
   }
-  return false;
+
+  // determine the actions that represent a diff between old and new arrays of
+  // lines, with an optional preference towards adding or removing lines
+  private function diff( $prefer = self::ADD ) {
+    // walk through both arrays individually
+    $old_idx = 0; $old_count = count($this->oldLines);
+    $new_idx = 0; $new_count = count($this->newLines);
+
+    $actions = array();
+
+    // loop until the end of one of both arrays is encountered
+    while( ( $old_idx < $old_count ) && ( $new_idx < $new_count ) ) {
+      // if the current lines are identical, mark so and advance both indexes
+      if( $this->oldLines[$old_idx] == $this->newLines[$new_idx] ) {
+        $actions[] = self::SAME;
+        $old_idx++;
+        $new_idx++;
+      } else {
+
+        // find shortest list of additions and deletions to reach a common
+        // line this is a Manhattan Distance problem, where additions and
+        // deletions are the two directions we can move in, but not all
+        // "streets" are open
+
+        // start at the currently reached point and take one step at a time
+        $depth = 0;
+        // worst case situation: remove all/add all remaining
+        $dels = count($this->oldLines) - $old_idx;
+        $adds = count($this->newLines) - $new_idx;
+
+        // keep looking for a better combo until the actual path ($dels+$adds)
+        // is smaller than the depth we're looking at
+        while( $depth * 2 < $dels + $adds ) {
+
+          // find out how many lines must be deleted to reach the same line
+          $odist = $this->look_for( $this->newLines[$new_idx+$depth],
+                                    $this->oldLines, $depth, $old_idx );
+
+          // find out how many lines should be added to reach the same line
+          if( $old_idx+$depth >= count($this->oldLines) ) {
+            $ndist = false;
+          } else {
+            $ndist = $this->look_for( $this->oldLines[$old_idx+$depth],
+                                      $this->newLines, $depth, $new_idx );
+          }
+
+          // if we prefer to ADD lines and we found a distance to a new lines
+          if( $prefer == self::ADD && $ndist) {
+            $dels = $depth;  // delete up to this depth
+            $adds = $ndist;  // step forward the distance
+          } elseif( $odist ) {
+            $dels = $odist;  // delete up to the distance
+            $adds = $depth;  // step forward the distance
+          }
+
+          // with the current dels and adds we reach the next same line
+          // step one step down, to see if a better combination comes up
+          // if there is a next one (we found a line before the end), we might
+          // find out that the next line is found closer (e.g. swapped lines)
+          // we prefer this shorter path
+          $depth++;
+        }
+
+        // move the indexes according to the shortest path
+        $old_idx += $dels;
+        $new_idx += $adds;
+
+        // and add actions
+        while( $dels > 0 ) { $actions[] = self::DEL; $dels--; }
+        while( $adds > 0 ) { $actions[] = self::ADD; $adds--; }
+      }
+    }
+
+    // the end of one array is reached, add actions to reach the other end
+    while( $old_idx < $old_count ) {
+      $actions[] = self::DEL;   // old items that have been removed
+      $old_idx++;
+    }
+    while( $new_idx < $new_count ) {
+      $actions[] = self::ADD;   // new items that have been added
+      $new_idx++;
+    }
+      
+    return $actions;
+  }
+
+  // looks for a line in an array starting at an index inceased with an offset
+  private function look_for( $line, $array, $start, $offset ) {
+    for( $d=$start; $d<count($array)-$offset; $d++ ) {
+      if( $array[$d+$offset] == $line ) {
+        return $d;
+      }
+    }
+    return false;
+  }
 }
 
-// returns the actions that represent a diff between old and new arrays of
-// lines, with an optional preference towards adding or removing lines
-function diff( $old, $new, $prefer = ADD ) {
-  // walk through both arrays individually
-  $old_idx = 0; $old_count = count($old);
-  $new_idx = 0; $new_count = count($new);
+class diffLine {
+  private $type;
+  private $string = "";
+  private $oldIndex = null, $newIndex = null;
   
-  // build a list of actions indicating if an identical line, a deleted or 
-  // added line was encountered
-  $actions = array();
-  
-  // loop until the end of one of both arrays is encountered
-  while( ( $old_idx < $old_count ) && ( $new_idx < $new_count ) ) {
-    // if the current lines are identical, mark so and advance both indexes
-    if( $old[$old_idx] == $new[$new_idx] ) {
-      $actions[] = SAME;
-      $old_idx++;
-      $new_idx++;
-    } else {
-      // find shortest list of additions and deletions to reach a common line
-      // this is a Manhattan Distance problem, where additions and deletions
-      // are the two directions we can move in, but not all "streets" are open
-
-      // start at the currently reached point and take one step at a time
-      $depth = 0;
-      // worst case situation: remove all/add all remaining
-      $dels = count($old) - $old_idx;
-      $adds = count($new) - $new_idx;
-      
-      // keep looking for a better combo until the actual path ($dels +$adds)
-      // is smaller than the depth we're looking at
-      while( $depth * 2 < $dels + $adds ) {
-
-        // find out how many old lines must be deleted to reach the same line
-        $odist = look_for( $new[$depth+$new_idx], $old, $depth, $old_idx );
-
-        // find out how many new lines should be added to reach the same line
-        $ndist = look_for( $old[$old_idx+$depth], $new, $depth, $new_idx );
-
-        // if we prefer to ADD lines and we found a distance to a new lines
-        if( $prefer == ADD && $ndist) {
-          $dels = $depth; // delete up to this depth
-          $adds = $ndist;  // step forward the distance
-        } elseif( $odist ) {
-          $dels = $odist;   // delete up to the distance
-          $adds = $depth;  // step forward the distance
-        }
-        
-        // with the current dels and adds we reach the next same line
-        // step one step down, to see if a better combination comes up
-        // if there is a next one (we found a line before the end), we might
-        // find out that the next line is found closer (e.g. swapped lines)
-        // we prefer this shorter path
-        $depth++;
-      }
-      
-      // move the indexes according to the shortest path
-      $old_idx += $dels;
-      $new_idx += $adds;
-      
-      // and add actions
-      while( $dels > 0 ) { $actions[] = DEL; $dels--; }  # deleted elements 
-      while( $adds > 0 ) { $actions[] = ADD; $adds--; }  # added elements 
-    }
+  public function __construct($type, $string, $oldIndex, $newIndex) {
+    $this->type     = $type;
+    $this->string   = $string;
+    $this->oldIndex = $oldIndex + 1;
+    $this->newIndex = $newIndex + 1;
   }
   
-  // the end of one array is reached, add actions to reach the other end
-  while( $old_idx < $old_count ) {
-    $actions[] = DEL;               // old items that have been removed
-    $old_idx++;
-  }
-  while( $new_idx < $new_count ) {
-    $actions[] = ADD;               // new items that have been added
-    $new_idx++;
-  }
-  
-  return $actions;
+  public function isAddition() { return $this->type == 'add';     }
+  public function isDeletion() { return $this->type == 'del';     }
+  public function isContext()  { return $this->type == 'context'; }
+  public function isSkip()     { return $this->type == 'skip';    }
+
+  public function getOldIndex() { return $this->oldIndex; }
+  public function getNewIndex() { return $this->newIndex; }
+
+  public function __toString() { return $this->string; }
 }
--- example.php
+++ example.php
@@ -1,67 +1,81 @@
+<html>
+<head>
+<style>
+  TABLE      { border-collapse : collapse; }
+  TD         { padding         : 1px;
+	             white-space: pre;
+	             font-family: Menlo, Monaco;
+	             font-size: 9pt;
+	           }
+  TD.index   { text-align: right; 
+               background-color: #F7F7F7; color: #BBBBBB;
+               padding-right: 3px;}
+  TD.label   { padding-left: 3px; }
+  TD.line    {}
+  TR.add     { background-color: #D9FFE0; color: #008828; }
+  TR.del     { background-color: #FFEEEE; color: #C2003D; }
+  TR.skip    { background-color: #F7F7F7; color: #BBBBBB;    }
+  TR.context { background-color: white;   color: black;   }
+</style>
+</head>
+<body>
 <?php
 
 include_once 'diff.php';
 
-$old_string = <<<EOT
-  Begin ...
-  little file
-  something out
-  Hello world this
-  is a test
-  to see diff.php in action
-  let's see
-  if this might work
+$old = <<<EOT
+Begin ...
+little file
+is a test
+to see diff.php in action
+let's see
+if this might work
 EOT;
 
-$new_string = <<<EOT
-  Hello world this
-  Begin ...
-  little file
-  is a test
-  to see diff.php in action
-  let us see
-  now
-  if this might work
-  let's see
-  End ...
+$new = <<<EOT
+Hello world this
+Begin ...
+little file
+is a test
+to see diff.php in action
+let us see
+now
+if this might work
+let's see
+End ...
 EOT;
 
-$old = split( "\n", $old_string );
-$new = split( "\n", $new_string );
 
-// calculate the diff with both preferences
-$actions_add = diff( $old, $new, ADD );
-$actions_del = diff( $old, $new, DEL );
-
-// pick the set of actions that has the least number of actions
-$actions = count($actions_add) < count( $actions_del) 
-  ? $actions_add : $actions_del;
-
-// print out a simple table
-print "<table>";
-$o = 0;
-$n = 0;
-$i = 0;
-$a = 0;
-foreach( $actions as $action ) {
-  switch( $action ) {
-    case SAME:
-      print "<tr><td>$i</td><td></td><td>$old[$o]</td><td>$new[$n]</td></tr>";
-      $o++; $n++;
-      break;
-    case DEL:
-      print "<tr><td>$i</td><td>-</td><td>$old[$o]</td><td></td></tr>";
-      $o++;
-      $a++;
-      break;
-    case ADD:
-      print "<tr><td>$i</td><td>+</td><td></td><td>$new[$n]</td></tr>";
-      $n++;
-      $a++;
-      break;
+$diff = new diff( $old, $new );
+$diff->showContext(1);
+$html = "<table>\n";
+while($line = $diff->getNextLine()) {
+  if( $line->isAddition() ) {
+    $class = 'add';     $label = '+';
+    $o = '';    $n = $line->getNewIndex();
+  } elseif( $line->isDeletion() ) {
+    $class = 'del';     $label = '-';
+    $n = '';    $o = $line->getOldIndex();
+  } elseif( $line->isContext() ) {
+    $class = 'context'; $label = '';
+    $o = $line->getOldIndex();    $n = $line->getNewIndex();
+  } else { // ->isSkip()
+    $class = 'skip';    $label = '';
+    $o = '...';  $n = '...';
   }
-  $i++;
-}
-print "</table>";
+  $html .= <<<EOT
+<tr class="{$class}">
+  <td class="index">{$o}</td>
+  <td class="index">{$n}</td>
+  <td class="label">{$label}</td>
+  <td class="line">{$line}</td>
+</tr>
+EOT;
 
-print "$a actions were required<br>\n";
+}
+$html .= "</table>\n";
+
+print $html;
+?>
+</body>
+</html>
